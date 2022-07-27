f2693bd4d61b2be50bdbd3f90ce6cbca62d24199
Letícia Costa Mancini <leticia.mancini@ibm.com>
2022-07-27 18:45:02 -0300

    eventos data binding

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -8,11 +8,32 @@ import { CourseService } from './course.service';
 })
 //onInit passa a informação no momento de inicialização do componente
 export class CourseListComponent implements OnInit {
-  courses: Course[] = [];
+  // Boas práticas: variável que começa com _ significa que quer
+  // que ela exista apenas dentro desse componente
+  filtredCourses: Course[] = [];
+  _courses: Course[] = [];
+  _filterBy: string;
 
   constructor(private courseService: CourseService) {}
 
   ngOnInit(): void {
-    this.courses = this.courseService.retrieveAll();
+    this._courses = this.courseService.retrieveAll();
+    this.filtredCourses = this._courses;
+  }
+
+  set filter(value: string) {
+    this._filterBy = value;
+
+    this.filtredCourses = this._courses.filter(
+      (course: Course) =>
+        course.name
+          //toLocaleLowerCase faz ele não diferenciar letras minúsculas das maiúsculas
+          .toLocaleLowerCase()
+          .indexOf(this._filterBy.toLocaleLowerCase()) > -1
+    );
+  }
+
+  get filter() {
+    return this._filterBy;
   }
 }
