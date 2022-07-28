9bdfd0b6f43c2de3148e808607f5ad230efc2520
Letícia Costa Mancini <leticia.mancini@ibm.com>
2022-07-28 03:11:22 -0300

    requisicoes http

---
+++ course-manager/src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseListComponent } from './courses/course-list.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { StarComponent } from 'src/star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './error-404/error-404.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component,
+    CourseInfoComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    // forRoot -> assim que inicializa a app já carrega as rotas
+    RouterModule.forRoot([
+      {
+        path: 'courses',
+        component: CourseListComponent,
+      },
+      {
+        path: 'courses/info/:id',
+        component: CourseInfoComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full',
+      },
+      // ** -> quando não encontra a url
+      {
+        path: '**',
+        component: Error404Component,
+      },
+    ]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ course-manager/src/app/courses/course-list.component.ts
@@ -0,0 +1,51 @@
+import { Component, createComponent, OnInit } from '@angular/core';
+import { Course } from './course';
+import { CourseService } from './course.service';
+
+@Component({
+  // Agora que já definiu o RouterModule, não precisa mais do selector
+  // selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
+})
+//onInit passa a informação no momento de inicialização do componente
+export class CourseListComponent implements OnInit {
+  // Boas práticas: variável que começa com _ significa que quer
+  // que ela exista apenas dentro desse componente
+  filtredCourses: Course[] = [];
+  _courses: Course[] = [];
+  _filterBy: string;
+
+  constructor(private courseService: CourseService) {}
+
+  ngOnInit(): void {
+    this.retrieveAll();
+  }
+
+  retrieveAll(): void {
+    this.courseService.retrieveAll().subscribe({
+      // next -> se der certo, retorna..
+      next: (courses) => {
+        this._courses = courses;
+        this.filtredCourses = this._courses;
+      },
+      // se der erro..
+      error: (err) => console.log('Error', err),
+    });
+  }
+
+  set filter(value: string) {
+    this._filterBy = value;
+
+    this.filtredCourses = this._courses.filter(
+      (course: Course) =>
+        course.name
+          //toLocaleLowerCase faz ele não diferenciar letras minúsculas das maiúsculas
+          .toLocaleLowerCase()
+          .indexOf(this._filterBy.toLocaleLowerCase()) > -1
+    );
+  }
+
+  get filter() {
+    return this._filterBy;
+  }
+}
